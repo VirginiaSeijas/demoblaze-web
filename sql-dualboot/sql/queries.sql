3a7ffd55d5a3711f67639e9ec0386257b0e62454
Josalys <josyvms@gmail.com>
2024-03-17 21:14:19 +0100

    fix

--- sql-dualboot/sql/queries.sql
+++ sql-dualboot/sql/queries.sql
@@ -1,29 +1,29 @@
--- A. Nombre y número del agente con la orden de mayor monto (Amount)
+-- A. Name and number of the agent with the largest order (Amount)
 SELECT a.Name, a.Employee_Number
 FROM Agenttbl a
 JOIN Orderstbl o ON a.Id = o.Agent_Id
 ORDER BY o.Amount DESC
 LIMIT 1;
 
--- B. Monto total de todas las órdenes del agente Seif
+-- B. Total amount of all orders of agent "Seif"
 SELECT SUM(o.Amount)
 FROM Orderstbl o
 JOIN Agenttbl a ON o.Agent_Id = a.Id
 WHERE a.Name = 'Seif';
 
--- C. Listar todos los clientes sin órdenes
+-- C.  List all customers without orders
 SELECT *
 FROM Customertbl
 WHERE Id NOT IN (SELECT Customer_Id FROM Orderstbl);
 
--- D. Listar todos los agentes con más de un cliente
+-- D. List all agents with more than one customer
 SELECT a.Id, a.Name
 FROM Agenttbl a
 JOIN Orderstbl o ON a.Id = o.Agent_Id
 GROUP BY a.Id, a.Name
 HAVING COUNT(DISTINCT o.Customer_Id) > 1;
 
--- E. Listar todos los clientes con órdenes realizadas el día 11/19/2015
+-- E.List all customers with orders placed on "2015-11-19"
 SELECT c.*
 FROM Customertbl c
 JOIN Orderstbl o ON c.Id = o.Customer_Id
